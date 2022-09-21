9f9153f2dad036f882dee95fa599a3c63a220f7d
ssun0402 <109329261+ssun0402@users.noreply.github.com>
2022-09-21 22:22:32 +0900

    Create 2.11.py

---
+++ python_practice/2.11.py
@@ -0,0 +1,7 @@
+# 2.11
+
+print("It's really hot!")
+print('I said "Hell0!" to him.')
+print("He said \"What's there?\"")
+print("Newline character is \\n, Tab charater is \\t.")
+print("Working directory is C:\\workspace\\mywork.")
